f08e3f356eb833f47bf2fe7102d8d1884a5a96ad
Aman Jain <amajain3@publicisgroupe.net>
2019-11-14 12:34:35 +0530

    Implemented Design

---
+++ build/precache-manifest.ec41323c32982be8d082973902dd661f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51045c831a71c7e034ff416eda42578b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b50ba1401fe009d2ef2",
+    "url": "/static/css/main.180b377c.chunk.css"
+  },
+  {
+    "revision": "681590bb1e130c5308b1",
+    "url": "/static/js/2.4fc36a69.chunk.js"
+  },
+  {
+    "revision": "2b50ba1401fe009d2ef2",
+    "url": "/static/js/main.4aee22d2.chunk.js"
+  },
+  {
+    "revision": "cfffea5c176e1bbfbcb5",
+    "url": "/static/js/runtime-main.05b1d247.js"
+  }
+]);
--- src/feeds/lib/feeds.jsx
+++ src/feeds/lib/feeds.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import FeedsImport from './api';
+import Logo from '../../images/y18.gif';
+import UpVote from './upVote';
+import extractDomain  from 'extract-domain';
+import TimeAgo from 'react-timeago'
 
 class Feeds extends Component {
   constructor() {
@@ -7,16 +11,16 @@ class Feeds extends Component {
     this.state = {
       feeds: [],
       load: false,
-      page: 0
+      page: 0,
+      totalPages: 0
     }
-
-    this.nextItems = this.nextItems.bind(this);
   }
 
   componentDidMount() {
     this.getFeedsData(this.state.page)
   }
 
+  // Fetching Feed data
   getFeedsData = async (page) => {
     this.setState({load: true});
     let response = await FeedsImport(page);
@@ -24,33 +28,54 @@ class Feeds extends Component {
     if ( data.length !== 0) {
       this.setState({feeds: [...this.state.feeds, ...data.hits]})
     }
-    this.setState({load: false});
+    this.setState({load: false, totalPages: data.nbPages});
   }
 
+  // Pager
   nextItems = () => {
     this.getFeedsData(this.state.page + 1)
     this.setState({page: this.state.page + 1});
   }
 
+  // Extracting domain name from url
+  getDomain = (url) => {
+    if (url !== null) {
+      return extractDomain(url);
+    }
+  }
+
   feedsTemplate() {
     return (
-      <div className='feeds-wrapper'>
+      <div className='feeds'>
         {
           this.state.feeds.map((items) => {
             return (
               <ul key={items.objectID}>
-                <li className='num-comments'>{items.num_comments}</li>
-                <li className='up-score'>{items.relevancy_score}</li>
-                <li className='up-score'>{items.relevancy_score}</li>
-                <li className='up-score'>{`${items.title} `}</li>
+                <li className='num-comments'>{items.num_comments ? items.num_comments : 0}</li>
+                <li className='up-score'><UpVote score={items.relevancy_score} id={items.objectID} /></li>
+                <li className='title'>{items.title}
+                  <span className='base-url grey'>
+                    {items.url && 
+                      <a href={items.url}>({this.getDomain(items.url)}) </a>
+                    }
+                     by</span>
+                    <span>{`${items.author}`}</span>
+                    <span className='grey'>
+                      <TimeAgo date={items.created_at} />
+                    </span>
+                </li>
               </ul>
             )   
           })
         }
         {
-          this.state.load &&
+          this.state.totalPages !== this.state.page &&
           <>
-            <div className='loading'>Loading...</div>
+            <ul>
+              <li className='num-comments'></li>
+              <li className='up-score'></li>
+              <li className='title more' onClick={this.nextItems}>More</li>
+            </ul>
           </>
         }
       </div>
@@ -58,14 +83,21 @@ class Feeds extends Component {
   }
 
   render() {
-    console.log(this.state.feeds);
     return (
-      <>
+      <div className='feeds-wrapper'>
+        <ul className='header'>
+          <li className='logo'><img src={Logo} alt="load" /></li>
+          <li className='label top-label'>top</li>
+          <li className='label new-label'>new</li>
+        </ul>
         {
           this.feedsTemplate()
         }
-        <div className='more-items' onClick={this.nextItems}>More</div>
-      </>
+        {
+          this.state.load &&
+          <div className='loading'>Loading...</div>
+        }
+      </div>
     )
   }
 }
